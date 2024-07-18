817b3e71b50c8bee10ff24942366c196a8dcc683
maksymdidukh <maxym.didukh@logivations.com>
2024-07-18 22:54:12 +0300

    add Lexer::new(), define TokenTypes

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,7 +1,21 @@
 pub struct Lexer {
-
+    input: String,
+    input_size: u32,
+    pos: u32,
+    read_pos: u32,
+    character: char,
 }
 
 impl Lexer {
+    fn new(content: String) -> Lexer {
+        Lexer {
+            input: content.clone(),
+            input_size: content.len() as u32,
+            pos: 0,
+            read_pos: 0,
+            character: ' ',
+        }
+    }
+
     
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod lexer;
 mod parser;
 
+use std::env;
+use std::fs;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Not enough arguments");
+    }
+
+    let content = fs::read_to_string(args[1].clone()).expect("No such file");
+    println!("{}", content);
+
 }
