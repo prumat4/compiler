bac3780a86c5f31274062e21d884ac05b623fe3d
maksymdidukh <maxym.didukh@logivations.com>
2024-07-20 22:32:16 +0300

    add ast.rs

---
+++ src/parser/ast.rs
@@ -0,0 +1,18 @@
+/*
+
+programm is a set of statements
+
+here we will need 3 nodes:
+- root
+- statement 
+- expr
+
+
+to parse the let statement we need to parse 3 parts: let <ident> = <expr>;
+
+we will use currToken and peekToken to traverse the tokens
+we nee peekToken ot decide what to do with currToken, cause in most cases the info that currToken is
+
+
+
+*/
